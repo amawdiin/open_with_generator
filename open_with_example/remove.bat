3f1fdc90c49d49e8d70c7a7c9a5e65074e2c3877
amandin <123622161+amawdiin@users.noreply.github.com>
2024-01-15 22:02:07 +0600

    removed unnecessary stuff, updated image

--- open_with_example/remove.bat
+++
@@ -1,7 +0,0 @@
-
-@echo off
-
-reg delete HKEY_CLASSES_ROOT\Directory\background\shell\OpenWithExample /f
-reg delete HKEY_CLASSES_ROOT\*\shell\OpenWithExample /f
-reg delete HKEY_CLASSES_ROOT\Directory\shell\OpenWithExample /f
-    
